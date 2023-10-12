feb6af60f3132097f3c7fb340144959130042ebc
MashaelKQ <117913825+MashaelKQ@users.noreply.github.com>
2023-10-12 13:12:04 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+const database = require("./database/database.js"); 
+
+
+app.use('/', express.static(path.join(__dirname, 'public', 'login')));
+app.use('/signup', express.static(path.join(__dirname, 'public', 'signup')));
+
+app.post('/login', (req, res) => { //changed the app.get to app.post
+  const { username, password } = req.body;//changed the req.query to req.body
+  const user = {
+    username : username,
+    password : password
+  }
+  database.authenticate(user)
+  .then((result) => {
+    if(result.length > 0){
+      res.json(result);
+    }
+    else{
+      res.redirect('/?error=true');
+    }
+  }
+  )
+});
+
+app.post('/submitSignup', (req, res) => { //changed the app.get to app.post
+  const { username, password } = req.body; //changed the req.query to req.body
+  const user = {
+    username : username,
+    password : password
+  }
+  database.signup(user)
+  .then((result) => {
+    if(result){
+      res.json("user created! please login");
+    }
+    else{
+      res.redirect('/signup?error=true');
+    }
+  }
+  )
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
